7e30a1d416ce4d556f4ca736fe69a395c6412ff3
maxigarrett <maxyg22@outlook.es>
2023-08-01 11:15:04 -0300

    agregamos utility types y funcion de chekear la tarea terminada

--- ejercicios/todolistUtilityTypes/src/main.tsx
+++ ejercicios/todolistUtilityTypes/src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "./index.css";
-import "todomvc-app-css/index.css";
+import "todomvc-app-css/index.css"; //este index es de npm que instalamos no del index que trae por defecto react
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
